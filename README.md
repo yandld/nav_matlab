ba649f7759a9ea1cf4f1de7d31687f4727603757
Alex Yang <1453363089@qq.com>
2020-11-20 20:56:50 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-6. HiPNUC 惯性导航，组合导航，UWB与IMU融合算法验证
+# 超核电子 基于的matlab导航科学计算库
+
+
+
+| 目录                                   | 说明                         |
+| -------------------------------------- | ---------------------------- |
+| \library\nav_lib\ahrs_test             | AHRS测试                     |
+| \nav_matlab\library\nav_lib\allan_test | allan方差测试                |
+| \library\nav_lib\calbiration_test      | 加速度计和陀螺校准测试       |
+| \library\nav_lib\gps_kalman_test       | 纯GPS定位，采用卡尔曼滤波    |
+| \nav_lib\loose_gps_imu_test            | 15维经典ESKF GPS+IMU组合导航 |
+| \library\nav_lib\uwb_imu_fusion_test   | UWB+IMU EKF 紧组合导航       |
+| \library\nav_lib\uwb_test              | 纯UWB三角测距定位测试        |
+
+*
+
+
+
+
+
--- library/nav_lib/allan_test/allan_test.m
+++ library/nav_lib/allan_test/allan_test.m
@@ -21,10 +21,10 @@ close all;
 
 
 %% https://github.com/Aceinna/gnss-ins-sim
-% VRW单位:                    m/s/sqrt(hr)           'accel_vrw': np.array([0.3119, 0.6009, 0.9779]) * 1.0,
+% VRW 单位:                    m/s/sqrt(hr)           'accel_vrw': np.array([0.3119, 0.6009, 0.9779]) * 1.0,
 % 加速度零偏不稳定性:     m/s^(2)                 'accel_b_stability': np.array([1e-3, 3e-3, 5e-3]) * 1.0,
-% ARW单位:                    deg/sqrt(hr)           'gyro_arw': np.array([0.25, 0.25, 0.25]) * 1.0,
-% 角速度零偏稳定性:        deg/h                    'gyro_b_stability': np.array([3.5, 3.5, 3.5]) * 1.0,  
+% ARW 单位:                    deg/sqrt(hr)           'gyro_arw': np.array([0.25, 0.25, 0.25]) * 1.0,
+% 角速度零偏稳定性:        deg/h                     'gyro_b_stability': np.array([3.5, 3.5, 3.5]) * 1.0,  
 
 M = csvread('gyro-0.csv',1 ,0);
 gyroReading = M(:,1);
@@ -68,7 +68,7 @@ accelReading = M(:,1);
 
 
             
-%% 运行陀螺allan
+%% 运行陀螺 allan
 [avar1, tau1 , N, K, B] = ch_allan(gyroReading , Fs, true);
 fprintf('GYR: 零偏不稳定性                                                             %frad/s                    或   %fdeg/h \n', B, rad2deg(B)*3600);
 fprintf('GYR: 噪声密度(角度随机游走, ARW, Noise density)              %f(rad/s)/sqrt(Hz)     或  %f deg/sqrt(h)\n', N, rad2deg(N)*3600^(0.5));
@@ -82,3 +82,6 @@ fprintf('GYR: 角速率随机游走, bias variations ("random walks")       %f(rad/s)sq
 fprintf('ACC: 零偏不稳定性                                                                                       %fm/s^(2)                       或   %fmg  或  %fug\n', B, B / 9.80665 *1000,  B / 9.80665 *1000*1000);
 fprintf('ACC: 噪声密度(速率随机游走,VRW, Noise Density, Rate Noise Density)          %f(m/s^(2))/sqrt(Hz)        或   %f m/s/sqrt(hr)\n', N, N * 3600^(0.5));
 fprintf('ACC: 加速度随机游走，bias variations ("random walks")                               %f(m/s^(2)sqrt(Hz))\n',  K);
+
+
+
