a6ef5586142cfdf1df137b873d69f03106a8697e
Alex Yang <1453363089@qq.com>
2021-04-06 13:31:49 +0800

    update

--- example/ins_test/example_ins5.m
+++ example/ins_test/example_ins5.m
@@ -1,12 +1,14 @@
 clear;
 clc;
 close all;
-format
+format long
 
+load filter.mat
 %% 读取数据
-data = csvread("UranusData6.csv", 1, 1);
+data = csvread("UranusData.csv", 1, 1);
 acc = data(:,2:4);
 gyr = data(:,5:7);
+mag = data(:,8:10);
 
 % acc = data(:,8:10);
 % gyr = data(:,11:13);
@@ -18,17 +20,14 @@ Fs = 100;
 dt = 1 / Fs;
 N = length(acc);
 
-    
 fprintf("共%d数据，用时:%.3fs\n", N, N/Fs);
 
 gyr = deg2rad(gyr);
 acc = acc*9.8;
 
-
 % 惯导解算, 初始化
 p = zeros(3, 1);
 v = zeros(3, 1);
-
 
 for i=1:N
     [p ,v , q] = ch_nav_equ_local_tan(p, v, q, acc(i,:)', gyr(i,:)', 1 / Fs, [0, 0, -9.8]');
@@ -38,42 +37,40 @@ for i=1:N
     R(:,:,i) = ch_q2m(q);
 end
 
-%线性速度
+% 速度
 linVel = zeros(size(linAcc));
 for i = 2:length(linAcc)
     linVel(i,:) = linVel(i-1,:) + linAcc(i,:) * dt;
 end
 
- %% High-pass filter linear velocity to remove drift
-
 order = 1;
 filtCutOff = 0.1;
-[b, a] = butter(order, (2*filtCutOff)/(1/dt), 'high');
-linVelHP = filtfilt(b, a, linVel);
+%[b, a] = butter(order, (2*filtCutOff)/(1/dt), 'high');
+%linVelHP = filtfilt(b, a, linVel);
+linVelHP = filter(Hd, linVel);
 
-
-
-figure('NumberTitle', 'off', 'Name', '速度');
-subplot(2,1,1);
-plot(linVel);
-title('线性速度');
-legend('X', 'Y', 'Z');
-subplot(2,1,2);
-plot(linVelHP);
-title('HP后线性速度');
-legend('X', 'Y', 'Z');
-
-
+% 位置
 linPos = zeros(size(linVelHP));
-
 for i = 2:length(linVelHP)
     linPos(i,:) = linPos(i-1,:) + linVelHP(i,:) * dt;
 end
 
-order = 1;
-filtCutOff = 0.1;
-[b, a] = butter(order, (2*filtCutOff)/(1/dt), 'high');
-linPosHP = filtfilt(b, a, linPos);
+%[b, a] = butter(order, (2*filtCutOff)/(1/dt), 'high');
+%linPosHP = filtfilt(b, a, linPos);
+linPosHP = filter(Hd, linPos);
+
+
+%% Plot
+figure('NumberTitle', 'off', 'Name', '速度');
+subplot(2,1,1);
+plot(linVel);
+title('速度');
+legend('X', 'Y', 'Z');
+subplot(2,1,2);
+plot(linVelHP);
+title('HP后速度');
+legend('X', 'Y', 'Z');
+
 
 
 figure('NumberTitle', 'off', 'Name', '位置');
@@ -87,7 +84,6 @@ title('HP后位置');
 legend('X', 'Y', 'Z');
 
 
-
 figure('NumberTitle', 'off', 'Name', '原始数据');
 subplot(2,1,1);
 plot(acc);
@@ -99,6 +95,8 @@ title("陀螺");
 legend("X", "Y", "Z");
 
 % 3D位置
+%linPosHP = mag;
+
 figure('NumberTitle', 'off', 'Name', '3D位置');
 plot3(linPosHP(1,1), linPosHP(1,2), linPosHP(1,3), '-ks');
 hold on;
