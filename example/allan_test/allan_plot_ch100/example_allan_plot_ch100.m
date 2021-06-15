62aa951fcb617a7e6058d0e65726fe4cae46b627
Alex Yang <1453363089@qq.com>
2021-06-15 17:49:22 +0800

    update allan example

--- example/allan_test/allan_plot_ch100/example_allan_plot_ch100.m
+++ example/allan_test/allan_plot_ch100/example_allan_plot_ch100.m
@@ -2,27 +2,37 @@ clear;
 clc;
 close all;
 
-%%  ch00   deg/s    m/s^(2)
-
-%data = csvread("UranusData.csv", 1, 1);
-
-
 load('ch100.mat');
-gyroReading = (gyroReading)*3600;
-accelReading = accelReading *  1000;
+gyroReading = imu.gyr * 3600;
+accelReading = imu.acc *  1000;
 Fs = 400;
+
+% load('hi226_hi229.mat');
+% gyroReading = imu.gyr * 3600;
+% accelReading = imu.acc *  1000;
+% Fs = 400;
+
+
+% load('../stim300.mat');
+% gyroReading = imu.gyr;
+% accelReading = imu.acc;
+% 
+% %转换为 deg/h,  m/s^(2)
+% gyroReading = gyroReading*3600;
+% accelReading = accelReading *1000;
+% Fs = 100;
 
 figure
 
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,1) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,1) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 hold on;
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,2) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,2) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,3) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,3) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 title('Gyroscope -- Allan variance');
@@ -33,17 +43,16 @@ grid on
 
 
 
-    
 figure
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,1) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,1) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 hold on;
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,2) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,2) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,3) , Fs, false);
+[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,3) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 legend('X','Y','Z');
--- lib/ch_allan.m
+++ lib/ch_allan.m
@@ -1,32 +1,30 @@
 
 %%  ('NoiseDensity(角度随机游走)', N, 'RandomWalk(角速率随机游走)', K,'BiasInstability(零偏稳定性)', B);
-% omega 必须为 rad/s
+% omega 必须为 rad/s(陀螺)， 加速度计: G
 % see Freescale AN: Allan Variance: Noise Analysis for Gyroscopes
-function [avar, tau, N, K, B] = ch_allan(omega, Fs, is_plot)
+function [avar, tau, N, K, B] = ch_allan(omega, Fs)
 
-%[avar, tau] = allanvar(omega, 'octave', Fs);
+% t0 = 1/Fs;
+% theta = cumsum(omega, 1)*t0;
+% 
+% maxNumM = 200;
+% L = size(theta, 1);
+% maxM = 2.^floor(log2(L/2));
+% m = logspace(log10(10), log10(maxM), maxNumM).';
+% m = ceil(m); % m must be an integer.
+% m = unique(m); % Remove duplicates.
+% 
+% tau = m*t0;
+% 
+% avar = zeros(numel(m), 1);
+% for i = 1:numel(m)
+%     mi = m(i);
+%     avar(i,:) = sum( ...
+%         (theta(1+2*mi:L) - 2*theta(1+mi:L-mi) + theta(1:L-2*mi)).^2, 1);
+% end
+% avar = avar ./ (2*tau.^2 .* (L - 2*m));
 
-t0 = 1/Fs;
-theta = cumsum(omega, 1)*t0;
-
-maxNumM = 200;
-L = size(theta, 1);
-maxM = 2.^floor(log2(L/2));
-m = logspace(log10(10), log10(maxM), maxNumM).';
-m = ceil(m); % m must be an integer.
-m = unique(m); % Remove duplicates.
-
-tau = m*t0;
-
-avar = zeros(numel(m), 1);
-for i = 1:numel(m)
-    mi = m(i);
-    avar(i,:) = sum( ...
-        (theta(1+2*mi:L) - 2*theta(1+mi:L-mi) + theta(1:L-2*mi)).^2, 1);
-end
-avar = avar ./ (2*tau.^2 .* (L - 2*m));
-
-
+[avar,tau] = allanvar(omega, 'octave', Fs);
 adev  = sqrt(avar);
 
 
@@ -76,7 +74,9 @@ slope = 0;
 logtau = log10(tau);
 logadev = log10(adev);
 dlogadev = diff(logadev) ./ diff(logtau);
+
 [~, i] = min(abs(dlogadev - slope));
+[~, i] = min(adev); %yandld added
 
 % Find the y-intercept of the line.
 b = logadev(i) - slope*logtau(i);
@@ -93,20 +93,16 @@ lineB = B * scfB * ones(size(tau));
 
 
 
-if is_plot == true
-    
-    tauParams = [tauN, tauK, tauB];
-    params = [N, K, scfB*B];
-    figure
-    loglog(tau, adev, tau, [lineN, lineK, lineB], '--',     tauParams, params, 'o');
-    
-    title('Allan Deviation with Noise Parameters')
-    xlabel('\tau')
-    ylabel('\sigma(\tau)')
-    legend('\sigma', '\sigma_N', '\sigma_K', '\sigma_B')
-    text(tauParams, params, {'N', 'K', '0.664B'})
-    grid on
-    axis equal
-end
+tauParams = [tauN, tauK, tauB];
+params = [N, K, scfB*B];
+% figure;
+% loglog(tau, adev, '-*', tau, [lineN, lineK, lineB], '--',  tauParams, params, 'o');
+% 
+% title('Allan Deviation with Noise Parameters')
+% xlabel('\tau')
+% ylabel('\sigma(\tau)')
+% legend('\sigma', '\sigma_N', '\sigma_K', '\sigma_B')
+% text(tauParams, params, {'N', 'K', '0.664B'})
+% grid on
 
 end
