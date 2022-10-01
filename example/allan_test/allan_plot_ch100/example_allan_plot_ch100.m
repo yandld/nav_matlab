b8eb4cb6ef0b5b6e86cf104ea60b267b84bba636
alex.yang <alex.yang@nxp.com>
2022-10-01 11:18:30 +0800

    code clean up

--- example/allan_test/allan_plot_ch100/example_allan_plot_ch100.m
+++ example/allan_test/allan_plot_ch100/example_allan_plot_ch100.m
@@ -3,36 +3,36 @@ clc;
 close all;
 
 
+%%
+file = 'bmi088x4.mat';
 
-
-load('ch010.mat');
+load(file);
 gyroReading = gyroReading * 3600;
 accelReading = accReading *  1000;
 Fs = 100;
+N = length(gyroReading);
 
+fprintf("数据共:%d，采样率:%dHz，时间:%d hr\r\n", N, Fs, N / Fs/3600);
 
-(length(gyroReading) / Fs) / 3600
+%%
+half = zeros(ceil(N/2), 3);
 
-% load('../stim300.mat');
-% gyroReading = imu.gyr;
-% accelReading = imu.acc;
-% 
-% %转换为 deg/h,  m/s^(2)
-% gyroReading = gyroReading*3600;
-% accelReading = accelReading *1000;
-% Fs = 100;
+for i=1:N/2
+    half(i,:) = gyroReading(2*i-1,:) + gyroReading(2*i,:)*1;
+end
+Fs = 50;
 
+%%
 figure
-
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,1) , Fs);
+[avar, tau] = ch_allan(gyroReading(:,1) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 hold on;
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,2) , Fs);
+[avar, tau] = ch_allan(gyroReading(:,2) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 
-[avar, tau , ~, ~, ~] = ch_allan(gyroReading(:,3) , Fs);
+[avar, tau] = ch_allan(gyroReading(:,3) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 title('Gyroscope -- Allan variance');
@@ -44,15 +44,15 @@ grid on
 
 
 figure
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,1) , Fs);
+[avar, tau] = ch_allan(accelReading(:,1) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 hold on;
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,2) , Fs);
+[avar, tau] = ch_allan(accelReading(:,2) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 
-[avar, tau , ~, ~, ~] = ch_allan(accelReading(:,3) , Fs);
+[avar, tau] = ch_allan(accelReading(:,3) , Fs);
 adev  = sqrt(avar);
 loglog(tau, adev);
 legend('X','Y','Z');
